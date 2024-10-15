8bf8ee0f825da7eb92208bd1a1626171f9aac0ba
GustavoFavorito <gustavofavorito.d@gmail.com>
2024-10-15 11:17:18 -0300

    Removendo arquivos com encode incorreto

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atentobr_n3_automacao_teste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Robo robo = new();
+            robo.ProcessaRobo();
+        }
+    }
+}
